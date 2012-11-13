69baf2dbe990f75dfcac0c35214bf42b94b01182
gabrielferra <gabrielferra@ig.com.br>
2012-11-13 18:25:59 +0000

    Pegando valores dos campos e dando set nos objeto, verificar logica para medidas

--- WorkOutSystem/src/workoutsystem/view/GUICriarExercicio.java
+++ WorkOutSystem/src/workoutsystem/view/GUICriarExercicio.java
@@ -2,16 +2,21 @@ package workoutsystem.view;
 
 import java.util.ArrayList;
 
+import workoutsystem.model.Exercicio;
 import workoutsystem.utilitaria.TipoExercicio;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class GUICriarExercicio extends Activity{
 
 	private Spinner cbxGrupo;
+	private EditText editNomeExercicio;
+	private EditText editDescricaoExercicio;
+
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,8 @@ public class GUICriarExercicio extends Activity{
 		setContentView(R.layout.criarexercicio);
 		cbxGrupo = (Spinner) findViewById(R.id.cbx_grupo);
 		criarCombo();
+		editNomeExercicio = (EditText) findViewById(R.id.edt_nomeExercicio);
+		editDescricaoExercicio = (EditText) findViewById(R.id.edt_descricaoExercicio);
 	}
 
 	public void criarCombo(){
@@ -37,4 +44,42 @@ public class GUICriarExercicio extends Activity{
 
 	}
 
+	public void criarExercicio(){
+		Exercicio exercicio = new Exercicio();
+
+		exercicio.setNomeExercicio(editNomeExercicio.getText().toString());
+
+		if(cbxGrupo.getSelectedItem().equals(TipoExercicio.Peito)){
+			exercicio.setGrupoMuscular(TipoExercicio.Peito.toString());
+		}else
+			if(cbxGrupo.getSelectedItem().equals(TipoExercicio.Costa)){
+				exercicio.setGrupoMuscular(TipoExercicio.Costa.toString());
+			}
+			else
+				if(cbxGrupo.getSelectedItem().equals(TipoExercicio.Biceps)){
+					exercicio.setGrupoMuscular(TipoExercicio.Biceps.toString());
+				}else
+					if(cbxGrupo.getSelectedItem().equals(TipoExercicio.Triceps)){
+						exercicio.setGrupoMuscular(TipoExercicio.Triceps.toString());
+					}
+					else
+						if(cbxGrupo.getSelectedItem().equals(TipoExercicio.Aerobico)){
+							exercicio.setGrupoMuscular(TipoExercicio.Aerobico.toString());
+						}
+						else
+							if(cbxGrupo.getSelectedItem().equals(TipoExercicio.Ombro)){
+								exercicio.setGrupoMuscular(TipoExercicio.Ombro.toString());
+							}
+							else
+								if(cbxGrupo.getSelectedItem().equals(TipoExercicio.MembrosInferiores)){
+									exercicio.setGrupoMuscular(TipoExercicio.MembrosInferiores.toString());
+								}else
+									if(cbxGrupo.getSelectedItem().equals(TipoExercicio.Abdomen)){
+										exercicio.setGrupoMuscular(TipoExercicio.Abdomen.toString());
+									}
+
+		exercicio.setDescricao(editDescricaoExercicio.getText().toString());
+		exercicio.setPersonalizado(true);
+
+	}
 }

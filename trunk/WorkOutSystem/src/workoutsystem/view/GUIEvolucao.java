1e2cdb7509699b18cff87a442dfd8c4121069483
caioportugal92@gmail.com <caioportugal92@gmail.com@0315515e-e2ae-3d5c-6cb2-450889f23a81>
2013-05-15 02:56:13 +0000

    Correção no preenchimento na barra de evolução !

--- trunk/WorkOutSystem/src/workoutsystem/view/GUIEvolucao.java
+++ trunk/WorkOutSystem/src/workoutsystem/view/GUIEvolucao.java
@@ -3,6 +3,7 @@ package workoutsystem.view;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 
 import workoutsystem.control.ControleMedida;
@@ -11,6 +12,7 @@ import workoutsystem.model.Exercicio;
 import workoutsystem.model.Medicao;
 import workoutsystem.model.Medida;
 import workoutsystem.model.Perfil;
+import workoutsystem.model.Treino;
 import workoutsystem.utilitaria.AdaptadorHistorico;
 import workoutsystem.utilitaria.ItemListaHistorico;
 import android.app.Activity;
@@ -39,23 +41,23 @@ public class GUIEvolucao extends Activity  {
 	private List<Medicao> listaMedicoes;
 	private int indice;
 	private ListView listahistorico;
-	
+
 	private TabHost hostEvolucao;
 	private TabSpec tabEvolucao;
 	private TabSpec tabHistorico;
-	
+
 	private ArrayAdapter<AdaptadorHistorico> adapter;
 	private AdaptadorHistorico adapterListView;
 	private List<ItemListaHistorico> itens;
-	
-	
-//	private ListView listView; - ok
-//    private AdapterListView adapterListView; - 
-//    private ArrayList<ItemListView> itens;
- 
 
 
-	
+	//	private ListView listView; - ok
+	//    private AdapterListView adapterListView; - 
+	//    private ArrayList<ItemListView> itens;
+
+
+
+
 	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
 
@@ -100,21 +102,21 @@ public class GUIEvolucao extends Activity  {
 	}
 
 	public void criarTabs(){
-		
+
 		hostEvolucao = (TabHost) findViewById(R.id.hostevolucao);
 		hostEvolucao.setup();
-		
+
 		tabEvolucao = hostEvolucao.newTabSpec("tabEvolucao");
 		tabEvolucao.setContent(R.id.tabEvolucao);
 		tabEvolucao.setIndicator("Evolução");
 		hostEvolucao.addTab(tabEvolucao);
-		
+
 		tabHistorico = hostEvolucao.newTabSpec("tabHistorico");
 		tabHistorico.setContent(R.id.tabHistorico);
 		tabHistorico.setIndicator("Historico");
 		hostEvolucao.addTab(tabHistorico);
-		
-		
+
+
 	}
 
 	public void onClick(View e) {
@@ -188,118 +190,126 @@ public class GUIEvolucao extends Activity  {
 		barra3.setProgress(0);
 
 		calcularProgresso(medida);
-		
-		
+
+
 		// criando ListView
 		listaMedicoes = controlemedida.buscarListaMedicaoes(medida.getCodigo(),perfil.getCodigo());
-		
+
 		createListView(listaMedicoes, listahistorico, medida);
-		
+
 
 	}
 
-	
-	
-	
+
+
+
 	private void calcularProgresso(Medida medida) {
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-
 		int maior = 70;
 		int medio = 50;
 		int menor = 30;
 		int contador = 0;
-		int naux = 2;
+		int naux = 1;
+		HashMap<Integer, Integer> valores = 
+			new HashMap<Integer, Integer>();
+		List<Medicao> list = medida.getMedicao();
+		Collections.sort(list,new ControleMedida());
 
-		//Collections.sort(medida.getMedicao(),new ControleMedida());
+		for (Medicao m : list){
+			if(contador == 0){
+				valores.put(list.get(contador).getCodigo(),menor);
+			}else if (contador == 1){
+				valores.put(list.get(contador).getCodigo(),medio);
+			}else if (contador == 2){
+				valores.put(list.get(contador).getCodigo(), maior);
+			}
+			contador = contador + 1;
+		}
 
+		contador = 0;
 
-		while (naux <= medida.getMedicao().size()){
+		while (naux <= list.size()){
 			if (naux == 2){
-				if (medida.getMedicao().get(contador).getValor()== 
-					medida.getMedicao().get(contador+1).getValor()){
-					medio = maior;
+				if (list.get(contador).getValor()
+						== list.get(contador+1).getValor()){
+					valores.remove(list.get(contador).getCodigo());
+					valores.put(list.get(contador).getCodigo(), medio);
 				} 
 			}else if (naux== 3){
-				if (medida.getMedicao().get(contador+1).getValor() == 
-					medida.getMedicao().get(contador+2).getValor()){
-					medio = menor;
+				if (list.get(contador+1).getValor()
+						== list.get(contador+2).getValor()){
+					valores.remove(list.get(contador+2).getCodigo());
+					valores.put(list.get(contador+2).getCodigo(),medio);
 				}
-				if (medida.getMedicao().get(contador).getValor() == 
-					medida.getMedicao().get(contador +1).getValor() &&
-						medida.getMedicao().get(contador+1).getValor()== 
-							medida.getMedicao().get(contador+2).getValor()){
-					menor = medio;
-					maior = medio;
-				}
-
 			}
 			naux++;
 
 		}
-		//Collections.reverse(medida.getMedicao());
-
+		contador = 0;
 
 		while (contador != medida.getMedicao().size()){
 			Medicao m = medida.getMedicao().get(contador);
 			String aux  =  sdf.format(m.getDataMedicao())+
 			" \n" + m.getValor()
 			+" "+ medida.getUnidade();
+			int valor = valores.get(m.getCodigo());
 			if (contador == 0){
 				data1.setText(aux);
-				barra1.setProgress(maior);
+				barra1.setProgress(valor);
 			}else if (contador == 1){
 				data2.setText(aux);
-				barra2.setProgress(medio);
+				barra2.setProgress(valor);
 			}else if (contador == 2){
 				data3.setText(aux);
-				barra3.setProgress(menor);
+				barra3.setProgress(valor);
 			}
 			contador ++;
 		}
-		
-		
+
+
+
+
 	}
 
-	
-	
-	
+
+
 
 	private void createListView(List <Medicao> medicoes, ListView lista, Medida medida) {
-		
 
-		
-		
+
+
+
 		ArrayList<Medicao> listaMedicoes = new ArrayList<Medicao>();
-		
-		itens = new ArrayList<ItemListaHistorico>();
-		
 
-		
+		itens = new ArrayList<ItemListaHistorico>();
+
+
+
 		for (Medicao m : medicoes){
 
-	
-			
+
+
 			String data = sdf.format(m.getDataMedicao());
 			//String valor = String.valueOf(m.getValor()+ " "+medida.getUnidade());
 			String valor = String.valueOf(m.getValor());
 			String unidade = medida.getUnidade();
-			
+
 			ItemListaHistorico item = new ItemListaHistorico(valor, data, unidade);
-			
+
 			itens.add(item);
-			
+
 
 		}
-		
+
 		adapterListView = new AdaptadorHistorico(this, itens);
 		lista.setAdapter(adapterListView);
-		
-		
+
+
 		lista.setCacheColorHint(Color.TRANSPARENT);
-		
+
 	}
-	
-	
+
+
 }
 
 

916a989c236def0fd0d2a80d150b1132d5644f9a
gabrielferra <gabrielferra@ig.com.br>
2013-07-18 13:33:19 +0000

    historico iniciado

--- WorkOutSystem/src/workoutsystem/dao/SerieDao.java
+++ WorkOutSystem/src/workoutsystem/dao/SerieDao.java
@@ -4,17 +4,21 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
 
 import workoutsystem.model.Exercicio;
+import workoutsystem.model.Ficha;
 import workoutsystem.model.Grupo;
+import workoutsystem.model.Realizacao;
 import workoutsystem.model.Serie;
+import workoutsystem.model.Treino;
 
 public class SerieDao implements ISerieDao {
-	
-	
+
+
 	@Override
 	public int buscarQuantidadeSerie(long codigoTreino) throws SQLException {
 		Connection con = ResourceManager.getConexao();
@@ -48,8 +52,8 @@ public class SerieDao implements ISerieDao {
 		prepare.close();
 		return resultado>0;
 	}
-	
-	
+
+
 	@Override
 	public boolean excluirSerieCodigo(long codigo) throws SQLException {
 		int aux = 1;
@@ -85,7 +89,7 @@ public class SerieDao implements ISerieDao {
 		int aux = 1;
 		int resultado = 0;
 		Connection con = ResourceManager.getConexao();
-		
+
 		String sql = 
 			"   update serie" +
 			"   set codigoexercicio = ?,codigotreino = ? ,ordem = ? ," +
@@ -110,7 +114,7 @@ public class SerieDao implements ISerieDao {
 
 
 	}
-	
+
 	@Override
 	public boolean removerSerie(long codigoTreino, long codigoExercicio) throws SQLException{
 		int aux = 1;
@@ -124,7 +128,7 @@ public class SerieDao implements ISerieDao {
 		prepare.close();
 		return (valor>=0);
 	}
-	
+
 
 
 	@Override
@@ -139,15 +143,15 @@ public class SerieDao implements ISerieDao {
 		prepare.close();
 		return (valor>=0);
 	}
-	
+
 	@Override
 	public boolean inserirRealizacaoSerie(Serie serie) throws SQLException{
 		int aux = 1;
 		int resultado = 0;
 		Connection con = ResourceManager.getConexao();
 		String sql = "insert into realizacaoSerie " +
-				" (codigoserie,codigotreino,codigoexercicio, ordem,unidade,carga, quantidade)" +
-				"values (?,?,?,?,?,?,?);";
+		" (codigoserie,codigotreino,codigoexercicio, ordem,unidade,carga, quantidade)" +
+		"values (?,?,?,?,?,?,?);";
 
 		PreparedStatement prepare = con.prepareStatement(sql);
 
@@ -158,14 +162,14 @@ public class SerieDao implements ISerieDao {
 		prepare.setString(aux++, serie.getUnidade());
 		prepare.setDouble(aux++, serie.getCarga());
 		prepare.setInt(aux++, serie.getQuantidade());
-		
+
 		resultado = prepare.executeUpdate();
 		prepare.close();
 		con.close();
 		return resultado>0;
 	}
 
-	
+
 
 	@Override
 	public boolean removerRealizacaoSerie(Serie serie) throws SQLException{
@@ -180,22 +184,58 @@ public class SerieDao implements ISerieDao {
 		return (valor>=0);
 	}
 
+	@Override
+	public boolean removerTodasRealizacoesSerie() throws SQLException{
+		int aux = 1;
+		Connection con = ResourceManager.getConexao();
+		String sql = "delete from realizacaoSerie ";
+		PreparedStatement prepare = con.prepareStatement(sql);
+		
+		int valor = prepare.executeUpdate();
+		con.close();
+		prepare.close();
+		return (valor>=0);
+	}
+
 	
 	
-	
+
+	@Override
+	public int buscarTreinoIniciado()	throws SQLException {
+
+		String sql = " select codigotreino from serie_realizacao ";
+		int resultado = 0;
+		Connection con = ResourceManager.getConexao();
+		PreparedStatement prepare = con.prepareStatement(sql);
+		ResultSet result = prepare.executeQuery();
+
+		while (result.next()){
+
+			resultado = result.getInt("codigotreino");
+
+		}
+
+		prepare.close();
+		con.close();
+
+		return resultado;
+	}
+
+
+
 	@Override
 	public List<Serie> listarRealizacaoSerie()	throws SQLException {
-		
+
 		String sql = " select codigoserie,codigotreino ,ordem,carga, unidade, quantidade, nome from serie_realizacao ";
-		
+
 		Connection con = ResourceManager.getConexao();
 		PreparedStatement prepare = con.prepareStatement(sql);
 		ResultSet result = prepare.executeQuery();
 		List<Serie> list = new ArrayList<Serie>();
 		Exercicio exercicio = new Exercicio();
-		
+
 		while (result.next()){
-						Serie serie = new Serie();
+			Serie serie = new Serie();
 			serie.setCodigo(result.getInt("codigoserie"));
 			serie.setCodigoTreino(result.getInt("codigotreino"));
 			serie.setOrdem(result.getInt("ordem"));
@@ -204,8 +244,8 @@ public class SerieDao implements ISerieDao {
 			serie.setQuantidade(result.getInt("quantidade"));
 			exercicio.setNome(result.getString("nome"));
 			serie.setExercicio(exercicio);
-			
-			
+
+
 			list.add(serie);
 
 		}
@@ -214,29 +254,68 @@ public class SerieDao implements ISerieDao {
 		con.close();
 		return list;
 	}
+	
+	@Override
+	public List<Realizacao> listarHistoricoRealizacaoSerie()	throws Exception {
+
+		String sql = " select distinct ficha.[nome] as ficha, treino.[nome] as treino,  datarealizacao from realizacao" +
+				" inner join ficha on ficha.[codigo] = realizacao.codigoficha " +
+				"inner join treino on treino.[codigo] = realizacao.codigotreino";
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Connection con = ResourceManager.getConexao();
+		PreparedStatement prepare = con.prepareStatement(sql);
+		ResultSet result = prepare.executeQuery();
+		
+		List<Realizacao> list = new ArrayList<Realizacao>();
+		
+		
+		while (result.next()){
+			Realizacao realizacao = new Realizacao();
+			Ficha ficha = new Ficha();
+			Treino treino = new Treino();
+
+			
+			ficha.setNome(result.getString("ficha"));
+			realizacao.setFicha(ficha);
+			
+			treino.setNome(result.getString("treino"));
+			realizacao.setTreino(treino);
+			
+			realizacao.setData(sdf.parse(result.getString("datarealizacao")));
+			
+			list.add(realizacao);
+
+		}
+
+		prepare.close();
+		con.close();
+		return list;
+	}
+
 
 	@Override
 	public boolean inserirRealizacao(Serie serie, long codigoFicha) throws SQLException {
-		
-	
+
+
 		int aux = 1;
 		int resultado = 0;
 		Connection con = ResourceManager.getConexao();
 		String sql = "insert into realizacao" +
-				"(datarealizacao,codigoserie, codigotreino, codigoficha)" +
-				"values (?,?,?,?);";
+		"(datarealizacao,codigoserie, codigotreino, codigoficha)" +
+		"values (?,?,?,?);";
 
 		PreparedStatement prepare = con.prepareStatement(sql);
 
 		java.util.Date data = new java.util.Date(); 
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String dataFormat = sdf.format(data); 
-		
+
 		prepare.setString(aux++, dataFormat);
 		prepare.setLong(aux++, serie.getCodigo());
 		prepare.setLong(aux++, serie.getCodigoTreino());
 		prepare.setLong(aux++, codigoFicha);
-		
+
 		resultado = prepare.executeUpdate();
 		prepare.close();
 		con.close();
@@ -244,27 +323,27 @@ public class SerieDao implements ISerieDao {
 
 	}
 
-	
-	
+
+
 	@Override
 	public List<Serie> listarSerie(long codigoTreino)
 	throws SQLException {
 		int aux = 1;
 		String sql =" select serie_codigo,serie_ordem, serie_repeticao," +
-					" serie_carga,serie_unidade, " +
-					" exercicio_codigo,exercicio_nome, exercicio_descricao," +
-					" exercicio_ativo,exercicio_padrao, " +
-					" grupo_nome, grupo_codigo, treino_codigo, " +
-					" ficha_codigo " +
-					" from serie_exercicio_treino " +
-					" where treino_codigo = ? order by  serie_ordem asc";
-		
+		" serie_carga,serie_unidade, " +
+		" exercicio_codigo,exercicio_nome, exercicio_descricao," +
+		" exercicio_ativo,exercicio_padrao, " +
+		" grupo_nome, grupo_codigo, treino_codigo, " +
+		" ficha_codigo " +
+		" from serie_exercicio_treino " +
+		" where treino_codigo = ? order by  serie_ordem asc";
+
 		Connection con = ResourceManager.getConexao();
 		PreparedStatement prepare = con.prepareStatement(sql);
 		prepare.setLong(aux++, codigoTreino);
 		ResultSet result = prepare.executeQuery();
 		List<Serie> list = new ArrayList<Serie>();
-		
+
 		while (result.next()){
 			Serie serie = new Serie();
 			serie.setCodigo(result.getInt("serie_codigo"));
@@ -273,22 +352,22 @@ public class SerieDao implements ISerieDao {
 			serie.setUnidade(result.getString("serie_unidade"));
 			serie.setCodigoTreino(result.getLong("treino_codigo"));
 			serie.setQuantidade(result.getInt("serie_repeticao"));
-			
+
 			Exercicio ex = new Exercicio();
 			ex.setNome(result.getString("exercicio_nome"));
 			ex.setCodigo(result.getLong("exercicio_codigo"));
 			ex.setDescricao(result.getString("exercicio_descricao"));
 			ex.setAtivo(result.getInt("exercicio_ativo"));
-			
+
 			serie.setExercicio(ex);
-			
-			
+
+
 			Grupo gr = new Grupo();
 			gr.setCodigo(result.getInt("grupo_codigo"));
 			gr.setNome(result.getString("grupo_nome"));
-			
+
 			ex.setGrupo(gr);
-			
+
 			list.add(serie);
 
 		}
@@ -344,13 +423,13 @@ public class SerieDao implements ISerieDao {
 		Connection con = ResourceManager.getConexao();
 		String sql = "delete from serie where codigoExercicio=?";
 		PreparedStatement prepare = con.prepareStatement(sql);
-		
+
 		prepare.setLong(aux++, codigo);
 		int valor = prepare.executeUpdate();
 		con.close();
 		prepare.close();
 		return (valor>=0);
-	
+
 	}
 
 
---
+++ WorkOutSystem/src/workoutsystem/model/Realizacao.java
@@ -0,0 +1,45 @@
+package workoutsystem.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Realizacao implements Serializable {
+	
+	private int codigo;
+	private Date data;
+	private Serie serie;
+	private Treino treino;
+	private Ficha ficha;
+	
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	public Serie getSerie() {
+		return serie;
+	}
+	public void setSerie(Serie serie) {
+		this.serie = serie;
+	}
+	public Treino getTreino() {
+		return treino;
+	}
+	public void setTreino(Treino treino) {
+		this.treino = treino;
+	}
+	public Ficha getFicha() {
+		return ficha;
+	}
+	public void setFicha(Ficha ficha) {
+		this.ficha = ficha;
+	}
+
+}
